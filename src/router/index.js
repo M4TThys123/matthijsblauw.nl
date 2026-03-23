a197a00332c5c8ea3b705699f924b024ce21f0fd
Matthijs Blauw <70703948+M4TThys123@users.noreply.github.com>
2026-03-23 13:08:05 +0100

    🔀 Routes: /contact, /blog, /projecten/:id toegevoegd.

--- src/router/index.js
+++ src/router/index.js
@@ -7,8 +7,23 @@ import ChartsPage from '@/pages/ChartsPage.vue'
 const routes = [
   { path: '/', name: 'Home', component: HomePage },
   { path: '/projecten', name: 'Projecten', component: ProjectenPage },
+  {
+    path: '/projecten/:id',
+    name: 'ProjectDetail',
+    component: () => import('@/pages/ProjectDetailPage.vue'),
+  },
   { path: '/werkervaring', name: 'Werkervaring', component: WerkervaringPage },
   { path: '/charts', name: 'Charts', component: ChartsPage },
+  {
+    path: '/blog',
+    name: 'Blog',
+    component: () => import('@/pages/BlogPage.vue'),
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import('@/pages/ContactPage.vue'),
+  },
 ]
 
 const router = createRouter({
